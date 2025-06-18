296340e39c471d535284677e02379b7a7efc7df3
pro1tocol <kngihtterror@qq.com>
2025-06-19 00:01:27 +0800

    Update window manager

---
+++ zsh/USER/config/i3/sh/touchpad.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+deviceid=`xinput | grep "Touchpad" | awk -F" " {'print $6'} | awk -F"=" {'print $2'}`
+propid=`xinput list-props ${deviceid} | grep "libinput Tapping Enabled (" | awk -F" " {'print $4'}`
+propid=${propid:1:3}
+xinput set-prop ${deviceid} ${propid} 1
